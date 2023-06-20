f2df059b466a1d8e88cf062f766b7d334ae32ea8
la0ura <134504965+la0ura@users.noreply.github.com>
2023-06-20 11:09:47 +0300

    Delete back-end directory

--- back-end/subscription.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once "../back-end/connect.php";
-
-$email = $_POST["email"];
-
-$sql = "INSERT INTO $tSubscription (`e-mail`) VALUES ('$email')";
-// Execute the SQL statement
-$db->query($sql);
-?>
